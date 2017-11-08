1d40646324aef96a6de5a4508adfba9484192f00
Reinaldo Haynes <reinaldo@hedgeable.com>
2017-11-08 11:10:27 -0500

    Component style update

--- src/components/SampleView/SampleView.js
+++ src/components/SampleView/SampleView.js
@@ -3,10 +3,22 @@ import React from 'react'
 export default class SampleView extends React.Component {
   render() {
     return (
-      <div>
-        <h3>{this.props.message}</h3>
-        <p>{this.props.label} {this.props.responseAction}</p>
+      <div style={styles.container}>
+        <div>
+          <h3>{this.props.message}</h3>
+          <p>{this.props.label} {this.props.responseAction}</p>
+        </div>
       </div>
     )
+  }
+}
+
+const styles = {
+  container: {
+    'min-height': '100vh',
+    'text-align': 'center',
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-items': 'center'
   }
 }
